939b6416b2a380814f3f249ba8cd1254246f0aaf
Rashmiranjan shaw <heyyrashmiranjan02@gmail.com>
2026-03-11 11:20:16 +0530

    added day3

--- src/com/qsp/day3/PrimeNumber.java
+++ src/com/qsp/day3/PrimeNumber.java
@@ -47,5 +47,23 @@ class PrimeNumber2 {
 	}
 }
 
+//  Advanced level to check the prime number
+class PrimeNumber3 {
+	public static void main(String[] args) {
+		System.out.println("Enter number ");
+		int n = new Scanner(System.in).nextInt();
+		System.out.println(solution(n));
+	}
+	public static boolean solution(int n) {
+		if (n <= 1)
+			return false;
+		for (int i = 2; i*i<=n; i++) { 
+			if (n % i == 0) {
+				return false;
+			}
+		}
+		return true;
+	}
+}
 
 
