51286669c5547e6350db92c7430b196be5ec2f57
Rashmiranjan shaw <heyyrashmiranjan02@gmail.com>
2026-03-11 11:14:02 +0530

    added day3'

---
+++ src/com/qsp/day3/PrimeNumber.java
@@ -0,0 +1,51 @@
+package com.qsp.day3;
+
+import java.util.Scanner;
+
+//Write the program to check input n is prime or not 
+class PrimeNumber {
+	public static void main(String[] args) {
+		System.out.println("Enter number");
+		int n=new Scanner(System.in).nextInt();
+		System.out.println(soluction(n));
+	}
+
+	public static boolean soluction(int n) {
+		if (n <= 1)
+			return false;
+		int count = 0;
+		for (int i = 1; i <= n; i++) {
+			if (n % i == 0)
+				count++;
+		}
+		return count==2;
+//		if (count == 2)
+//			return true;
+//		else
+//			return false;
+	}
+
+}
+
+// another method 
+
+class PrimeNumber2 {
+	public static void main(String[] args) {
+		System.out.println("Enter number ");
+		int n = new Scanner(System.in).nextInt();
+		System.out.println(soluction(n));
+	}
+	public static boolean soluction(int n) {
+		if (n <= 1)
+			return false;
+		for (int i = 2; i < n / 2; i++) {
+			if (n % i == 0) {
+				return false;
+			}
+		}
+		return true;
+	}
+}
+
+
+
