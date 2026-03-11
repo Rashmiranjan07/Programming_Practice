a9347f5bfefe78def262ddb2597f6933bf18c0c4
Rashmiranjan shaw <heyyrashmiranjan02@gmail.com>
2026-03-11 11:45:38 +0530

    added the day3

--- src/com/qsp/day3/CheckPrimeNumber.java
+++ src/com/qsp/day3/CheckPrimeNumber.java
@@ -1,6 +1,6 @@
 package com.qsp.day3;
 
-public class CheckPrimeNumber {
+class CheckPrimeNumber {
 	public static void main(String[] args) {
 		for (int i = 1; i <= 100; i++) {
 			if(soluction(i)) {
@@ -20,5 +20,32 @@ public class CheckPrimeNumber {
 		}
 		return true;
 	}
+}
 
+
+// reverse
+
+class CheckPrimeNumber2 {
+	public static void main(String[] args) {
+		int count = 0;
+		for (int i = 100; i >= 1; i--) {
+			if (soluction(i)) {
+				System.out.println(i);
+				count++;
+			}
+		}
+		System.out.println("No of prime" + count);
+		System.out.println("No of non prime" + (100 - count));
+	}
+
+	public static boolean soluction(int n) {
+		if (n <= 1)
+			return false;
+		for (int i = 2; i < n / 2; i++) {
+			if (n % i == 0) {
+				return false;
+			}
+		}
+		return true;
+	}
 }
