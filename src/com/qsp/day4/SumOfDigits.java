cb6ab224b611ab91b6fd45a3b7551d2a102715fb
Rashmiranjan shaw <heyyrashmiranjan02@gmail.com>
2026-03-12 12:09:24 +0530

    added day4

--- src/com/qsp/day4/SumOfDigits.java
+++ src/com/qsp/day4/SumOfDigits.java
@@ -11,3 +11,19 @@ class SumOfDigits {
 	}
 	/* 55 */
 }
+
+
+class SumofDigits2 {
+	public static void main(String[] args) {
+		System.out.println(sum(11));
+
+	}
+
+	static int sum(int n) {
+		int res = 0;
+		for (int i = n; i >= 1; i--) {
+			res += i;
+		}
+		return res;
+	}
+}
