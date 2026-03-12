31156d37ec90b421bef8e8ba84b0f5c047fa672f
Rashmiranjan shaw <heyyrashmiranjan02@gmail.com>
2026-03-12 12:13:24 +0530

    added day4

--- src/com/qsp/day4/SumOfDigits.java
+++ src/com/qsp/day4/SumOfDigits.java
@@ -28,3 +28,19 @@ class SumofDigits2 {
 	}
 	/* 66 */
 }
+
+
+
+// By Recursion formulae 
+class SumofDigits3 {
+	public static void main(String[] args) {
+		System.out.println(sum(10));
+
+	}
+
+	static int sum(int n) {
+		if (n == 0)
+			return 0;
+		return n + sum(n - 1);
+	}
+}
