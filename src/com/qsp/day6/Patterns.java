bbde311a502f26e50a667f0a819548730b82dd3c
Rashmiranjan shaw <heyyrashmiranjan02@gmail.com>
2026-03-16 12:01:13 +0530

    added day6

--- src/com/qsp/day6/Patterns.java
+++ src/com/qsp/day6/Patterns.java
@@ -27,3 +27,17 @@ class Patterns {
 	 */
 
 }
+
+class pattern2 {
+	public static void main(String[] args) {
+		System.out.println("Enter no of row");
+		int n = new Scanner(System.in).nextInt();
+		for (int i = 1; i <= n; i++) {
+			for (int j = 1; j <= i; j++) {
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+
+	}
+}
