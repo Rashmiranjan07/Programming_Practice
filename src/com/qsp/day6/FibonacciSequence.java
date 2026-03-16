ec90083d12e751bbd8dbf40604bc03dd21e8147b
Rashmiranjan shaw <heyyrashmiranjan02@gmail.com>
2026-03-16 11:14:28 +0530

    added day6

--- src/com/qsp/day6/FibonacciSequence.java
+++ src/com/qsp/day6/FibonacciSequence.java
@@ -1,5 +1,30 @@
 package com.qsp.day6;
 
-public class FibonacciSequence {
+class FibonacciSequence {
+	public static void main(String[] args) {
+		fibonacci(10);
+	}
+
+	static void fibonacci(int n) {
+		int a = 0;
+		int b = 1;
+		for (int i = 1; i <= n; i++) {
+			System.out.println(a);
+			int c = a + b;
+			a = b;
+			b = c;
+		}
+
+	}
+	/*
+1
+2
+3
+5
+8
+13
+21
+34
+	 */
 
 }
