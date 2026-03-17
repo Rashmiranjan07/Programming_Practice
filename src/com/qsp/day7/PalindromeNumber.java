5220f28f75e2341719bbd638be82def628a1f0f6
Rashmiranjan shaw <heyyrashmiranjan02@gmail.com>
2026-03-17 11:08:04 +0530

    added

--- src/com/qsp/day7/PalindromeNumber.java
+++ src/com/qsp/day7/PalindromeNumber.java
@@ -6,7 +6,12 @@ class PalindromeNumber {
 	public static void main(String[] args) {
 		System.out.println("Enter a number");
 		int n = new Scanner(System.in).nextInt();
+		int num = n;
+		int rev = 0;
+		int rem;
 		
 	}
+	 
+	
 
 }
