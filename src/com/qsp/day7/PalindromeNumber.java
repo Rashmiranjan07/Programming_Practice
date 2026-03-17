0c6d006cd9ed2cd542abc10c8be566afa15a9d66
Rashmiranjan shaw <heyyrashmiranjan02@gmail.com>
2026-03-17 11:13:18 +0530

    added

--- src/com/qsp/day7/PalindromeNumber.java
+++ src/com/qsp/day7/PalindromeNumber.java
@@ -6,12 +6,19 @@ class PalindromeNumber {
 	public static void main(String[] args) {
 		System.out.println("Enter a number");
 		int n = new Scanner(System.in).nextInt();
-		int num = n;
-		int rev = 0;
-		int rem;
-		
+		System.out.println(soluction(n));
+
 	}
-	 
-	
+
+	static boolean soluction(int n) {
+		int temp = n;
+		int rev = 0;
+		while (n > 0) {
+			int rem = n % 10;
+			n /= 10;
+			rev = (rev * 10) + rem;
+		}
+		return rev == temp;
+	}
 
 }
