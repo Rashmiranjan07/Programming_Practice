eb0051fc413737ec3d0b227efe96396759f88f3e
Rashmiranjan shaw <heyyrashmiranjan02@gmail.com>
2026-03-17 11:39:04 +0530

    added

--- src/com/qsp/day7/NumberPattern.java
+++ src/com/qsp/day7/NumberPattern.java
@@ -24,3 +24,31 @@ Enter no of row
 1
 	 */
 }
+
+class NumberPattern2{
+	public static void main(String[] args) {
+		System.out.println("Enter no of row");
+		int n = new Scanner(System.in).nextInt();
+		for (int i = n; i >= 1; i--) {
+			for (int j = i; j >= 1; j--) {
+				System.out.print(j);
+			}
+			System.out.println();
+		}
+
+	}
+}
+
+class NumberPattern3{
+	public static void main(String[] args) {
+		System.out.println("Enter no of row");
+		int n = new Scanner(System.in).nextInt();
+		for (int i = n; i >= 1; i--) {
+			for (int j = i; j >= 1; j--) {
+				System.out.print(i);
+			}
+			System.out.println();
+		}
+
+	}
+}
