4d7125b5c997f6b22850572d90b6cbfec986c9ba
Rashmiranjan shaw <heyyrashmiranjan02@gmail.com>
2026-03-19 10:53:44 +0530

    added day9

--- src/com/qsp/day9/ArmstrongNumber.java
+++ src/com/qsp/day9/ArmstrongNumber.java
@@ -2,14 +2,34 @@ package com.qsp.day9;
 
 class ArmstrongNumber {
 	public static void main(String[] args) {
-		
+		System.out.println(isArmStrong(153));
 	}
 	
-	static int numberOfDigits(int n) {
+	static boolean isArmStrong(int n) {
+		int digitCount=numberOfDigit(n); // method call number of digits
+		int temp=n;
+		int sum=0;
+		while(n>0) {
+			int rem=n%10;
+			sum=sum+power(rem,digitCount); // power sum
+			n=n/10;
+		}
+		return temp==sum;
+	}
+	
+	static int numberOfDigit(int n) {
 		int count=0;
 		while(n>0) {
 			count++;
-			n/10;
+			n/=10;
+		}
+		return count;
+	}
+	
+	static int power(int x, int y) {
+		
+		
+			
 		}
 	}
 
