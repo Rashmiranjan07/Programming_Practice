0232a361d6e64134fe607f3830dd4fde9350e78b
Rashmiranjan shaw <heyyrashmiranjan02@gmail.com>
2026-03-19 11:36:21 +0530

    added day9

--- src/com/qsp/day9/Patterns.java
+++ src/com/qsp/day9/Patterns.java
@@ -2,7 +2,7 @@ package com.qsp.day9;
 
 import java.util.Scanner;
 
-class Pattern6 {
+class Pattern1 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter the value of n");
@@ -28,6 +28,22 @@ class Pattern6 {
      */
 }
 
+// Another way 
+ class Pattern2 {
+	public static void main(String[] args) {
+		System.out.print("Emter the number of row: ");
+		int n = new Scanner(System.in).nextInt();
+		for (int i = n; i >= 1; i--) {
+			for (int j = 1; j <= n - i; j++) {
+				System.out.print(" ");
+			}
+			for (int k = 1; k <= i; k++) {
 
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+	}
+}
 
 
