c849c12d449e5ed844c6206ea8f9a8b384f4d25b
Rashmiranjan shaw <heyyrashmiranjan02@gmail.com>
2026-03-19 11:33:44 +0530

    added

--- src/com/qsp/day8/Patterns.java
+++ src/com/qsp/day8/Patterns.java
@@ -77,3 +77,30 @@ Emter the number of row: 5
 	 */
 }
 
+ class Pattern4 {
+	public static void main(String[] args) {
+		System.out.print("Emter the number of row: ");
+		int n = new Scanner(System.in).nextInt();
+		for (int i = 1; i <= n; i++) {
+			for (int j = 1; j <= n - i; j++) {
+				System.out.print(" ");
+			}
+			for (int k = 1; k <= i; k++) {
+				if (i == 1 || i == n || i == k || k == 1)
+					System.out.print("*");
+				else
+					System.out.print(" ");
+			}
+			System.out.println();
+		}
+	}
+	/*
+Emter the number of row: 4
+   *
+  **
+ * *
+****
+
+	 */
+}
+
