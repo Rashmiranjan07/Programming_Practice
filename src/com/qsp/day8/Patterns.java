8d49d37dfe6bd6877697e749282aa5f657587e6b
Rashmiranjan shaw <heyyrashmiranjan02@gmail.com>
2026-03-19 11:17:29 +0530

    day8

--- src/com/qsp/day8/Patterns.java
+++ src/com/qsp/day8/Patterns.java
@@ -1,5 +1,27 @@
 package com.qsp.day8;
 
-class Patterns {
+import java.util.Scanner;
 
+ class Pattern1 {
+	public static void main(String[] args) {
+		System.out.print("Emter the number of row: ");
+		int n = new Scanner(System.in).nextInt();
+		for (int i = 1; i <= n; i++) {
+			for (int j = 1; j <= n - i; j++) {
+				System.out.print(" ");
+			}
+			for (int k = 1; k <= i; k++) {
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+	}
+	/*
+Emter the number of row: 5
+    *
+   **
+  ***
+ ****
+*****
+	 */
 }
