4fcdf900995d5ae652b41d51125012354d1a0ab1
Rashmiranjan shaw <heyyrashmiranjan02@gmail.com>
2026-03-19 11:56:38 +0530

    added day9

---
+++ src/com/qsp/day8/NoofDigits.java
@@ -0,0 +1,20 @@
+package com.qsp.day8;
+
+import java.util.Scanner;
+
+class NoofDigits {
+	public static void main(String[] args) {
+		System.out.print("Emter a number: ");
+		int n = new Scanner(System.in).nextInt();
+		System.out.println(solution(n));
+	}
+
+	static int solution(int n) {
+		int count = 0;
+		while (n > 0) {
+			n /= 10;
+			count++;
+		}
+		return count;
+	}
+}
--- src/com/qsp/day9/PyramidPattern.java
+++ src/com/qsp/day9/PyramidPattern.java
@@ -20,3 +20,36 @@ class PyramidPattern {
 	}
 
 }
+
+// another method 
+
+ class PyramidPattern2 {
+	public static void main(String[] args) {
+		System.out.print("Emter the number of row: ");
+		int n = new Scanner(System.in).nextInt();
+		int sp = n - 1;
+		int st = 1;
+		for (int i = 0; i < n; i++) {
+			for (int j = 0; j <= sp; j++) {
+				System.out.print(" ");
+			}
+			for (int k = 1; k <= st; k++) {
+
+				System.out.print("*");
+			}
+			sp--;
+			st += 2;
+			System.out.println();
+		}
+	}
+	/*
+Emter the number of row: 7
+       *
+      ***
+     *****
+    *******
+   *********
+  ***********
+ *************
+	 */
+}
