08b1ec2663fdd85816583b3cc91269f8221f7792
Rashmiranjan shaw <heyyrashmiranjan02@gmail.com>
2026-03-19 12:10:07 +0530

    added day9

--- src/com/qsp/day9/PyramidPattern.java
+++ src/com/qsp/day9/PyramidPattern.java
@@ -53,3 +53,40 @@ Emter the number of row: 7
  *************
 	 */
 }
+
+ 
+ // Hollow pyramid 
+ class PyramidPattern3 {
+		public static void main(String[] args) {
+			System.out.print("Emter the number of row: ");
+			int n = new Scanner(System.in).nextInt();
+			int sp = n - 1;
+			int st = 1;
+			for (int i = 1; i <= n; i++) {
+				for (int j = 0; j <= sp; j++) {
+					System.out.print(" ");
+				}
+				for (int k = 1; k <= st; k++) {
+					if (i == 1 || i == n || st == k || k == 1)
+						System.out.print("*");
+					else
+						System.out.print(" ");
+				}
+				sp--;
+				st += 2;
+				System.out.println();
+			}
+		}
+		/*
+Emter the number of row: 8
+        *
+       * *
+      *   *
+     *     *
+    *       *
+   *         *
+  *           *
+ ***************
+		 */
+	}
+ 
