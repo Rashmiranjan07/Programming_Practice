aa58279354f3d58e94404b7bed00b521a5642458
Rashmiranjan shaw <heyyrashmiranjan02@gmail.com>
2026-03-19 11:02:30 +0530

    added day9

--- src/com/qsp/day9/ArmstrongNumber.java
+++ src/com/qsp/day9/ArmstrongNumber.java
@@ -4,33 +4,35 @@ class ArmstrongNumber {
 	public static void main(String[] args) {
 		System.out.println(isArmStrong(153));
 	}
-	
+
 	static boolean isArmStrong(int n) {
-		int digitCount=numberOfDigit(n); // method call number of digits
-		int temp=n;
-		int sum=0;
-		while(n>0) {
-			int rem=n%10;
-			sum=sum+power(rem,digitCount); // power sum
-			n=n/10;
+		int digitCount = numberOfDigit(n); // method call number of digits
+		int temp = n;
+		int sum = 0;
+		while (n > 0) {
+			int rem = n % 10;
+			sum = sum + power(rem, digitCount); // power sum
+			n = n / 10;
 		}
-		return temp==sum;
+		return temp == sum;
 	}
-	
+
 	static int numberOfDigit(int n) {
-		int count=0;
-		while(n>0) {
+		int count = 0;
+		while (n > 0) {
 			count++;
-			n/=10;
+			n /= 10;
 		}
 		return count;
 	}
-	
-	static int power(int x, int y) {
-		
-		
-			
-		}
-	}
 
+	static int power(int x, int y) {
+		int res = 1;
+		for (int i = 1; i <= y; i++) {
+			res = res * x;
+
+		}
+		return res;
+	}
+// true 
 }
