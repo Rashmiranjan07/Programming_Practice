40f9a5f664e6bf0de456c041eaab53c427079444
Rashmiranjan shaw <heyyrashmiranjan02@gmail.com>
2026-03-19 10:45:21 +0530

    added

--- src/com/qsp/day9/ArmstrongNumber.java
+++ src/com/qsp/day9/ArmstrongNumber.java
@@ -1,5 +1,16 @@
 package com.qsp.day9;
 
 class ArmstrongNumber {
+	public static void main(String[] args) {
+		
+	}
+	
+	static int numberOfDigits(int n) {
+		int count=0;
+		while(n>0) {
+			count++;
+			n/10;
+		}
+	}
 
 }
