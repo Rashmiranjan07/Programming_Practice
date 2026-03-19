2a936b7f5ad1f1e54a5bd4504acfb14483d06edf
Rashmiranjan shaw <heyyrashmiranjan02@gmail.com>
2026-03-19 12:06:01 +0530

    added day8

--- src/com/qsp/day8/NoofDigits.java
+++ src/com/qsp/day8/NoofDigits.java
@@ -23,3 +23,37 @@ Emter a number: 23456
 
 	 */
 }
+
+
+
+class NumOfDigits2 {
+	public static void main(String[] args) {
+		System.out.println("Enter a number : ");
+		int n = new Scanner(System.in).nextInt();
+		System.out.println(solution(n));
+	}
+
+	static int solution(int n) {
+		return (n + "").length();
+	}
+}
+
+//======================================================
+class NumOfDigits3 {
+	public static void main(String[] args) {
+		System.out.println("Enter a numbers : ");
+		int n = new Scanner(System.in).nextInt();
+		System.out.println(solution(n));
+	}
+
+	static int solution(int n) {
+		if (n <= 9)
+			return 1;
+		return 1 + solution(n / 10);
+	}
+}
+/*
+Emter a number: 23456
+5
+
+ */
