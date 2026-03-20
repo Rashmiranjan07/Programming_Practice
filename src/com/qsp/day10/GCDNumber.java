cce9979c1453c269ee6263b0054930e69a00bc10
Rashmiranjan shaw <heyyrashmiranjan02@gmail.com>
2026-03-20 11:09:02 +0530

    added day10

--- src/com/qsp/day10/GCDNumber.java
+++ src/com/qsp/day10/GCDNumber.java
@@ -1,5 +1,17 @@
 package com.qsp.day10;
 
 class GCDNumber {
-
+	public static void main(String[] args) {
+		System.out.println(gcd(3, 93));
+	}
+	static int gcd(int n1, int n2) {
+		//int min = n1 > n2 ? n2 : n1;
+		int min=Math.min(n1, n2);
+		for (int i = min; i > 1; i--) {
+			if (n1 % i == 0 && n2 % i == 0) {
+				return i;
+			}
+		}
+		return 1;
+	}
 }
