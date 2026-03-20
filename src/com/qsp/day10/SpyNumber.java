edb5a66dc6647e314b9f6a2dce1b134795553c87
Rashmiranjan shaw <heyyrashmiranjan02@gmail.com>
2026-03-20 12:18:49 +0530

    added day10

---
+++ src/com/qsp/day10/SpyNumber.java
@@ -0,0 +1,24 @@
+package com.qsp.day10;
+
+import java.util.Scanner;
+
+public class SpyNumber {
+	public static void main(String[] args) {
+		System.out.println("Enter sequence number");
+		int n = new Scanner(System.in).nextInt();
+		System.out.println(isSpy(n));
+	}
+
+	static boolean isSpy(int n) {
+		int sum = 0;
+		int multiply = 1;
+		while (n > 0) {
+			int rem = n % 10;
+			n /= 10;
+			sum += rem;
+			multiply *= rem;
+		}
+		return sum == multiply;
+	}
+
+}
