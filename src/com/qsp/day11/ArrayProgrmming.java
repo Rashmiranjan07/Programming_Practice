e6de5d3dcc54650541b5b784727f174fdda5f8f6
Rashmiranjan shaw <heyyrashmiranjan02@gmail.com>
2026-03-23 12:11:20 +0530

    added day11

--- src/com/qsp/day11/ArrayProgrmming.java
+++ src/com/qsp/day11/ArrayProgrmming.java
@@ -1,6 +1,13 @@
 package com.qsp.day11;
 
 class CreateArray {
-	
+	public static void main(String[] args) {
+		int a[] = { 10, 12, 25, 8, 12 };
+		System.out.println(a[0]);
+		System.out.println(a[1]);
+		System.out.println(a[2]);
+		System.out.println(a[3]);
+		System.out.println(a[4]);
 
+	}
 }
