68da87afb2c23c870c50b9245140f661498d5843
Rashmiranjan shaw <heyyrashmiranjan02@gmail.com>
2026-03-24 11:23:50 +0530

    added day12

--- src/com/qsp/day12/For_each.java
+++ src/com/qsp/day12/For_each.java
@@ -2,10 +2,25 @@ package com.qsp.day12;
 
 class For_each {
 	public static void main(String[] args) {
-		int a[]= { 9,2,0,6,4};
+		int a[]= {9,2,0,6,4};
 		for( int x: a) {
 			System.out.println(x);
 		}
 	}
 
+}
+
+class SumOfElements {
+	public static void main(String[] args) {
+		int a[]= {9,2,0,6,4,1};
+		int sum = 0;
+		for (int num : a) {
+		    sum += num;
+		}
+		System.out.println(sum);
+		
+	}
+	/*
+22
+	 */
 }
