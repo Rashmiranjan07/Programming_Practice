8ef4ba81519b59fc2eb089ace5d0f425baeaacbd
Rashmiranjan shaw <heyyrashmiranjan02@gmail.com>
2026-03-24 12:00:49 +0530

    added day12

--- src/com/qsp/day12/For_each.java
+++ src/com/qsp/day12/For_each.java
@@ -75,7 +75,7 @@ class PrimeNumber {
 	static boolean isPrime(int n) {
 		if (n <= 1)
 			return false; // 1 and below are not prime
-		for (int i = 2; i*i<=n; i++) {
+		for (int i = 2;i*i<=n; i++) {
 			if (n % i == 0)
 				return false; // Found a divisor
 		}
