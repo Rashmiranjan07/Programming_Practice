ad66c26d943593f2d6951ff4f36cf9a2ec6816dc
Rashmiranjan shaw <heyyrashmiranjan02@gmail.com>
2026-03-26 11:33:38 +0530

    added day13

--- src/com/qsp/day13/MissingNumber.java
+++ src/com/qsp/day13/MissingNumber.java
@@ -1,5 +1,25 @@
 package com.qsp.day13;
 
-class MissingNumber {
+import java.util.Arrays;
 
+class MissingNumber {
+	public static void main(String[] args) {
+		int a[] = { 2, 4, 1, 5, 3, 7, 8, 9 };
+		missing1(a);
+	}
+
+	static void missing1(int a[]) {
+		Arrays.sort(a);   // using sort
+		for (int i = 1; i < a.length; i++) {
+			if (a[i] - 1 != a[i - 1]) {
+				System.out.println(a[i] - 1);
+			}
+		}
+/*
+6
+ */
+	}
 }
+
+
+// Without sorting an array 
