ec9abcb380c3d4324f9b6d38cbf5f1db816f8640
Rashmiranjan shaw <heyyrashmiranjan02@gmail.com>
2026-03-26 10:56:01 +0530

    added day13

--- src/com/qsp/day13/MaximumElement.java
+++ src/com/qsp/day13/MaximumElement.java
@@ -1,5 +1,21 @@
+//26-03-2026
 package com.qsp.day13;
 
-class MaximumElement {
+class MaxElement {
+	public static void main(String[] args) {
+		int a[] = { 2, 3, 9, 1, 5, 6 };
+		int res = max(a);
+		System.out.println(res);
+	}
+
+	static int max(int a[]) {
+		int max = Integer.MIN_VALUE;
+		for (int i = 0; i < a.length; i++) {
+			if (a[i] > max) {
+				max = a[i];
+			}
+		}
+		return max;
+	}
 
 }
