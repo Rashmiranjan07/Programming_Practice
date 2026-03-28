b06991b711c6538018691abfaa063dbc3d41f6f7
Rashmiranjan shaw <heyyrashmiranjan02@gmail.com>
2026-03-28 11:20:16 +0530

    added day14

--- src/com/qsp/day14/LinearSearch.java
+++ src/com/qsp/day14/LinearSearch.java
@@ -4,6 +4,8 @@ class LinearSearch {
 	public static void main(String[] args) {
 		int a[] = { 4, 2, 6, 1, 5, 3, 8 };
 		System.out.println(indexOf(a, 9));
+		System.out.println(lastIndexOf(a, 6));
+
 	}
 
 	static int indexOf(int a[], int key) {
@@ -14,7 +16,21 @@ class LinearSearch {
 		}
 		return -1;
 	}
-/*
- -1
- */
+	/*
+	 * -1
+	 */
+
+	static int lastIndexOf(int a[], int key) {
+		for (int i = a.length - 1; i >= 0; i--) {
+			if (a[i] == key) {
+				return i;
+			}
+		}
+		return -1;
+
+	}
+	
 }
+
+
+
