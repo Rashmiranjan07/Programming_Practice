48cc98cb83a0b9b1a0c557fce3ff184aaabd9194
Rashmiranjan shaw <heyyrashmiranjan02@gmail.com>
2026-03-28 11:36:25 +0530

    added day14

--- src/com/qsp/day14/UpdateArrayElement.java
+++ src/com/qsp/day14/UpdateArrayElement.java
@@ -1,11 +1,22 @@
 package com.qsp.day14;
+import java.util.Arrays;
 
 class UpdateArrayElement {
 	public static void main(String[] args) {
 		int a []= { 9,2,6,0,4,2,6,1 };
-		
+		System.out.println(Arrays.toString(a));
+		replace( a ,2, 10);
+		System.out.println(Arrays.toString(a));
 		
 	}
-	
-
+	  static void replace( int a[], int oldElement, int newElement) {
+		  for(int i=0; i<a.length;i++) {
+			  if(a[i]==oldElement) {
+				  a[i]=newElement;
+			  }
+		  }
+	  }
+/*
+[9, 2, 6, 0, 4, 2, 6, 1]
+[9, 10, 6, 0, 4, 10, 6, 1] */
 }
