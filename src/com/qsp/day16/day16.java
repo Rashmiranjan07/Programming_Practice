d679180dc6bb045990207006d017892bb2d60aa3
Rashmiranjan shaw <heyyrashmiranjan02@gmail.com>
2026-03-31 11:54:09 +0530

    added day16

--- src/com/qsp/day16/day16.java
+++ src/com/qsp/day16/day16.java
@@ -70,7 +70,6 @@ class DigitSum {
 			int count = 0;
 			for (int i = 0; i < s.length(); i++) {
 				char ch = s.charAt(i);
-
 				if (ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u')
 					count++;
 			}
