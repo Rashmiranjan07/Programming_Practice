c6c2f630a66bc2a4d59d4075a6974ab794cc4707
Rashmiranjan shaw <heyyrashmiranjan02@gmail.com>
2026-03-31 11:31:20 +0530

    added day16

--- src/com/qsp/day16/PrefixSum.java
+++ src/com/qsp/day16/PrefixSum.java
@@ -29,3 +29,25 @@ class TypeConversion {
 		}
 	}
 }
+
+
+class DigitSum {
+	public static void main(String[] args) {
+		String s="h12e5l2l5o7";
+		digitSum(s);
+	}
+	static int digitSum(String s) {
+		int sum=0;
+		for( int i=0; i<s.length(); i++) {
+			char ch=s.charAt(i);
+			if( ch>='0' && ch<='9') {
+				System.out.println(ch);
+				 sum = sum + (ch - '0'); // converted into '0' value is 48 
+				                         // we can converted it into appropiate number by -48 / - '0'
+			}
+		}
+		System.out.println(sum);
+		return 0;
+	}
+
+}
