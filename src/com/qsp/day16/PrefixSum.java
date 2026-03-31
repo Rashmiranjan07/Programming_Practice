4ac9d6a0be97e9be6eeef197e2f6bd64f7bd22e8
Rashmiranjan shaw <heyyrashmiranjan02@gmail.com>
2026-03-31 10:53:07 +0530

    added day16

--- src/com/qsp/day16/PrefixSum.java
+++ src/com/qsp/day16/PrefixSum.java
@@ -2,13 +2,7 @@ package com.qsp.day16;
 
 class PrefixSum {
 	public static void main(String[] args) {
-		int n= arr.length;
-		int [] prefix= new int[n];
-		prefix[0] = arr[0];
-		for(int i=1; i<n; i++) {
-			prefix[i] = prefix[i-1]+arr[i];
-		}
-		return prefix;
+	
 	}
 
 }
