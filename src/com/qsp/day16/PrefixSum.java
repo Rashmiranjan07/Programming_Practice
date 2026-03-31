199253fb211106c1895b021df741b64ba16cb734
Rashmiranjan shaw <heyyrashmiranjan02@gmail.com>
2026-03-31 10:59:35 +0530

    added day16

--- src/com/qsp/day16/PrefixSum.java
+++ src/com/qsp/day16/PrefixSum.java
@@ -1,8 +1,17 @@
 package com.qsp.day16;
 
+import java.util.Arrays;
+
 class PrefixSum {
 	public static void main(String[] args) {
-	
+		int a[] = { 9, 2, 6, 0, 4, 2 };
+		soluction(a);
+		System.out.println(Arrays.toString(a));
 	}
 
+	static void soluction(int a[]) {
+		for (int i = 1; i < a.length; i++) {
+			a[i] = a[i] + a[i - 1];
+		}
+	}
 }
