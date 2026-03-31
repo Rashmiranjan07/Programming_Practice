45a630586e2d2a6552e4265cb0137d4bff8ca01c
Rashmiranjan shaw <heyyrashmiranjan02@gmail.com>
2026-03-31 12:00:50 +0530

    added day16

--- src/com/qsp/day16/day16.java
+++ src/com/qsp/day16/day16.java
@@ -68,6 +68,7 @@ class DigitSum {
 		}
 		static int NoOfVowel(String s) {
 			int count = 0;
+			s=s.toLowerCase();
 			for (int i = 0; i < s.length(); i++) {
 				char ch = s.charAt(i);
 				if (ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u')
