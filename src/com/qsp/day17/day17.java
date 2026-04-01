0f6c21dc93a1c729188066a54efb95e43b167ad9
Rashmiranjan shaw <heyyrashmiranjan02@gmail.com>
2026-04-01 11:34:47 +0530

    added day17

--- src/com/qsp/day17/day17.java
+++ src/com/qsp/day17/day17.java
@@ -42,3 +42,45 @@ class MergeOfArray {
 	 */
 
 }
+
+
+class MergeArrayWithSort {
+	public static void main(String[] args) {
+		int a[] = { 1, 5, 7, 8 };
+		int b[] = { 2, 3, 4, 6, 9, 10 };
+		int res[]=soluction(a, b);
+		System.out.println(Arrays.toString(res));
+
+	}
+
+	static int[] soluction(int a[], int b[]) {
+
+		int c[] = new int[a.length + b.length];
+		int i = 0;
+		int j = 0;
+		int k = 0;
+		while (i < a.length && j < b.length) {
+			c[k] = a[i];
+			k++;
+			i++;
+			c[k] = b[j];
+			k++;
+			j++;
+		}
+		while (i < a.length) {
+			c[k] = a[i];
+			i++;
+			k++;
+		}
+		while (j < b.length) {
+			c[k] = b[j];
+			j++;
+			k++;
+		}
+		Arrays.sort(c);
+		return c;
+		
+	}
+}
+
+
