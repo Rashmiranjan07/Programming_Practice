27e0b8c66391627a02c0c52dc11521b0c81b4647
Rashmiranjan shaw <heyyrashmiranjan02@gmail.com>
2026-04-01 11:07:20 +0530

    added day17

--- src/com/qsp/day17/day17.java
+++ src/com/qsp/day17/day17.java
@@ -4,7 +4,7 @@ class MergeOfArray {
 	public static void main(String[] args) {
 		int a[]= {9,2,6,0};
 		int b[]= {4,5,6,1,3,7};
-		
+		 soluction(a,b);
 
 	}
 
@@ -13,7 +13,7 @@ class MergeOfArray {
 		int i=0;
 		int j=0;
 		int k=0;
-		while () {
+		while (i<a.length && i<b.length) {
 			c[k]=a[i];
 			k++; i++;
 			c[k]=b[j];
