a390a21b377785f551e7bc917fa536346095738e
Rashmiranjan shaw <heyyrashmiranjan02@gmail.com>
2026-04-01 11:10:48 +0530

    added day17

--- src/com/qsp/day17/day17.java
+++ src/com/qsp/day17/day17.java
@@ -1,29 +1,38 @@
 package com.qsp.day17;
 
+import java.util.Arrays;
+
 class MergeOfArray {
 	public static void main(String[] args) {
-		int a[]= {9,2,6,0};
-		int b[]= {4,5,6,1,3,7};
-		 soluction(a,b);
+		int a[] = { 9, 2, 6, 0 };
+		int b[] = { 4, 5, 6, 1, 3, 7 };
+		int res[] = soluction(a, b);
+		System.out.println(Arrays.toString(res));
 
 	}
 
 	static int[] soluction(int a[], int b[]) {
 		int c[] = new int[a.length + b.length];
-		int i=0;
-		int j=0;
-		int k=0;
-		while (i<a.length && j<b.length) {
-			c[k]=a[i];
-			k++; i++;
-			c[k]=b[j];
-			k++; j++;
+		int i = 0;
+		int j = 0;
+		int k = 0;
+		while (i < a.length && j < b.length) {
+			c[k] = a[i];
+			k++;
+			i++;
+			c[k] = b[j];
+			k++;
+			j++;
 		}
-		while(i<a.length) {
-			c[k]=a[i]; i++; k++;
+		while (i < a.length) {
+			c[k] = a[i];
+			i++;
+			k++;
 		}
-		while(j<b.length) {
-			c[k]=b[j]; j++; k++;
+		while (j < b.length) {
+			c[k] = b[j];
+			j++;
+			k++;
 		}
 		return c;
 
