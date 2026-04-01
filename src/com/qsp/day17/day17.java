448ed59daff61b043872908afb248b14963b6117
Rashmiranjan shaw <heyyrashmiranjan02@gmail.com>
2026-04-01 10:57:46 +0530

    added day17

--- src/com/qsp/day17/day17.java
+++ src/com/qsp/day17/day17.java
@@ -2,11 +2,17 @@ package com.qsp.day17;
 
 class MergeOfArray {
 	public static void main(String[] args) {
+		int a[]= {9,2,6,0};
+		int b[]= {4,5,6,1,3,7};
+		
 
 	}
 
 	static int[] soluction(int a[], int b[]) {
 		int c[] = new int[a.length + b.length];
+		for(int i=0; i<a.length; i++) {
+			
+		}
 		return c;
 
 	}
