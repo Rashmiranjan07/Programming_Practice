ae5d97d2589199ae4d9cde5fbc2f5c6cf5ea344a
Rashmiranjan shaw <heyyrashmiranjan02@gmail.com>
2026-04-01 12:15:14 +0530

    added day17

--- src/com/qsp/day17/day17.java
+++ src/com/qsp/day17/day17.java
@@ -129,6 +129,28 @@ class MergeArrayWithSort {
 	 */
 }
 
+class ReplceChar {
+	public static void main(String[] args) {
+		String s = "hello world";
+		String op=replcechar(s,'o','s');
+		System.out.println(op);
 
+	}
+
+	static String replcechar(String s, char oldChar, char newChar) {
+		char ch[] = s.toCharArray();
+
+		for (int i = 0; i < ch.length; i++) {
+			if (ch[i] == oldChar) {
+				ch[i] = newChar;
+			}
+		}
+
+		return new String(ch);
+	}
+/*
+ hells wsrld
+ */
+}
 
 
