6cd95664027b78327685cf0b3fdb236c6b539a3a
Rashmiranjan shaw <heyyrashmiranjan02@gmail.com>
2026-04-02 11:37:28 +0530

    added day18

--- src/com/qsp/day18/day18.java
+++ src/com/qsp/day18/day18.java
@@ -38,7 +38,24 @@ class FindKey2 {
 
 class Panagram {
 	public static void main(String[] args) {
-		
+		String str = "The quick brown fox jumps over the lazy dog";
+
+		if (isPanagram(str)) {
+			System.out.println("The given string is a Pangram.");
+		} else {
+			System.out.println("The given string is NOT a Pangram.");
+		}
 	}
-	
+
+	static boolean isPanagram(String s) {
+		for (char ch = 'a'; ch <= 'z'; ch++) {
+			if (s.indexOf(ch) == -1) {
+				return false;
+			}
+		}
+
+		return true;
+
+	}
+
 }
