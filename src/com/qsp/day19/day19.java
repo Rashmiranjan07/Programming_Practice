8c0b9265c08423dc2a57d51c1ac022662170f777
Rashmiranjan shaw <heyyrashmiranjan02@gmail.com>
2026-04-03 11:04:44 +0530

    added day19

--- src/com/qsp/day19/day19.java
+++ src/com/qsp/day19/day19.java
@@ -1,14 +1,26 @@
 package com.qsp.day19;
 
+import java.util.Arrays;
+
 // Write a java program to find out  the frequency of each character in a string 
 //( the string only contains lowercase character ) 
 
-class Frequency {
+class FrequencyOfChar {
 	public static void main(String[] args) {
-		
+		int a[] = new int[26];
+
 	}
-	
-	
-	
+
+	static void solution(String s) {
+		int a[] = new int[26];
+		for (int i = 0; i < s.length(); i++) {
+			int index = s.charAt(i) - 'a';
+			a[index]++;
+		}
+		System.out.println(Arrays.toString(a));
+		for (int i = 0; i < a.length; i++) {
+			System.out.println((char) (i + 'a') + " " + a[i]);
+		}
+	}
 
 }
