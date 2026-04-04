106718dbcd4de32f397475ae29e84a3d3d1f5519
Rashmiranjan shaw <heyyrashmiranjan02@gmail.com>
2026-04-04 10:55:56 +0530

    added day20

--- src/com/qsp/day20/day20.java
+++ src/com/qsp/day20/day20.java
@@ -1,5 +1,17 @@
 package com.qsp.day20;
 
-class day20 {
+import java.util.Arrays;
+
+class ReverseSentense {
+	public static void main(String[] args) {
+		String s="java is easy";
+		reverse(s);
+		
+	}
+	static String reverse(String s) {
+		String sa[]=s.split(" ");
+		System.out.println(Arrays.toString(sa));
+		return null;
+	}
 
 }
