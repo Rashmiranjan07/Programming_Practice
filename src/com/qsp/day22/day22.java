bb00559bf48783f7c71a775a4b689b55d9769bd5
Rashmiranjan shaw <heyyrashmiranjan02@gmail.com>
2026-04-07 10:52:31 +0530

    added day22

--- src/com/qsp/day22/day22.java
+++ src/com/qsp/day22/day22.java
@@ -1,5 +1,30 @@
 package com.qsp.day22;
 
-class day22 {
+import java.util.Arrays;
+import java.util.Scanner;
+
+class FibonacciSequence {
+	public static void main(String[] args) {
+		Scanner Sc = new Scanner(System.in);
+		System.out.println("Enter a number ");
+		int n = Sc.nextInt();
+		int[] result = fibonacci(n);
+		System.out.println("Fibonacci sequence in array:");
+		System.out.println(Arrays.toString(result));
+	}
+
+	static int[] fibonacci(int n) {
+		int[] arr = new int[n];
+		int a = 0;
+		int b = 1;
+		for (int i = 1; i < n; i++) {
+			System.out.println(a);
+			arr[i] = a;
+			int c = a + b;
+			a = b;
+			b = c;
+		}
+		return arr;
+	}
 
 }
