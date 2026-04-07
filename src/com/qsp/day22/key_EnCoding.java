589e5f52ed1d6808f8bb786a5847de53ebf26fe2
Rashmiranjan shaw <heyyrashmiranjan02@gmail.com>
2026-04-07 11:16:11 +0530

    added day22

--- src/com/qsp/day22/key_EnCoding.java
+++ src/com/qsp/day22/key_EnCoding.java
@@ -2,13 +2,18 @@ package com.qsp.day22;
 
 class key_EnCoding {
 	public static void main(String[] args) {
-		String s="abcdef";
-		sol(s,4);
+		String s = "abcdef";
+		sol(s, 4);
 	}
+
 	static String sol(String s, int key) {
-		for(int i=0; i<s.length();i++) {
-			char ch=s.charAt(i);
-			System.out.println(ch);
+		for (int i = 0; i < s.length(); i++) {
+			char ch = s.charAt(i);
+			char temp = (char) (ch + key);
+			if (temp > 'z') {
+
+			}
+			System.out.println(ch + "->" + temp);
 		}
 		return null;
 	}
