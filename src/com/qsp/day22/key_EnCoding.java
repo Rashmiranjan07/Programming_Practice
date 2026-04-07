306a167657ae0591cc69aa9a35999e086d1a00d7
Rashmiranjan shaw <heyyrashmiranjan02@gmail.com>
2026-04-07 11:27:20 +0530

    added day22

--- src/com/qsp/day22/key_EnCoding.java
+++ src/com/qsp/day22/key_EnCoding.java
@@ -3,35 +3,23 @@ package com.qsp.day22;
 class key_EnCoding {
 	public static void main(String[] args) {
 		String s = "abcdefuvwxyz";
-		sol(s, 4);
+		String res = sol(s, 4);
+		System.out.println(res);
 	}
 
 	static String sol(String s, int key) {
+		String res = "";
 		for (int i = 0; i < s.length(); i++) {
 			char ch = s.charAt(i);
 			char temp = (char) (ch + key);
 			if (temp > 'z') {
-				int used = 'z' - ch;  //calculate how many used
+				int used = 'z' - ch; // calculate how many used
 				int pending = key - used; // calculate how many pendinng
 				temp = (char) (96 + pending);
 			}
-			System.out.println(ch + "->" + temp);
+			res = res + temp;
 		}
-		return null;
+		return res;
 	}
-	/*
-a->e
-b->f
-c->g
-d->h
-e->i
-f->j
-u->y
-v->z
-w->a
-x->b
-y->c
-z->d
-	 */
 
 }
