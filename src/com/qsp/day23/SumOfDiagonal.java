65b28612a66c41f7b11c93c68f4a0024bc85ec6c
Rashmiranjan shaw <heyyrashmiranjan02@gmail.com>
2026-04-08 11:44:44 +0530

    added 23

--- src/com/qsp/day23/SumOfDiagonal.java
+++ src/com/qsp/day23/SumOfDiagonal.java
@@ -8,9 +8,16 @@ class SumOfDiagonal {
 				{2,3,9,1},
 				{8,3,5,4}
 				};
-		
+		int k=0; 
+		int j=a.length-1;
+		int sum=0;
+		for (int i = 0; i < a.length; i++) {
+			sum = sum + a[i][k];
+			sum = sum + a[i][j];
+			k++;
+			j--;
 		}
-	
+		System.out.println(sum);
+	}
+
 }
-
-
