1e684b02a14af69f0d2c70091fa170ab19fd1470
Rashmiranjan shaw <heyyrashmiranjan02@gmail.com>
2026-04-08 10:57:49 +0530

    added 23

--- src/com/qsp/day23/day23.java
+++ src/com/qsp/day23/day23.java
@@ -1,10 +1,18 @@
 package com.qsp.day23;
 
+import java.util.Arrays;
+
 class TwoDArray {
 	public static void main(String[] args) {
 		int n=5;
 		int a[][]=new int[n][];
+		for( int i=0; i<=a.length; i++) {
+			a[i]=new int[i+1];
+		}
+		for(int x[]:a) 
+			System.out.println(Arrays.toString(x));
+		}
 	}
 
-}
+
 
