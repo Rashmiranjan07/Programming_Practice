ad04f1848dfee231e327aaf8d54e6aed28fe0668
Rashmiranjan shaw <heyyrashmiranjan02@gmail.com>
2026-04-08 10:58:04 +0530

    added 23

--- src/com/qsp/day23/day23.java
+++ src/com/qsp/day23/day23.java
@@ -4,15 +4,12 @@ import java.util.Arrays;
 
 class TwoDArray {
 	public static void main(String[] args) {
-		int n=5;
-		int a[][]=new int[n][];
-		for( int i=0; i<=a.length; i++) {
-			a[i]=new int[i+1];
+		int n = 5;
+		int a[][] = new int[n][];
+		for (int i = 0; i <= a.length; i++) {
+			a[i] = new int[i + 1];
 		}
-		for(int x[]:a) 
+		for (int x[] : a)
 			System.out.println(Arrays.toString(x));
-		}
 	}
-
-
-
+}
