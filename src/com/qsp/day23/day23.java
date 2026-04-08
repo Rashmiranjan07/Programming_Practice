e4ee5726f381657e1aefb966c84a64cd6bfe096f
Rashmiranjan shaw <heyyrashmiranjan02@gmail.com>
2026-04-08 11:04:57 +0530

    added 23

--- src/com/qsp/day23/day23.java
+++ src/com/qsp/day23/day23.java
@@ -8,9 +8,9 @@ class TwoDArray {
 		int a[][] = new int[n][];
 		for (int i = 0; i < a.length; i++) {
 			a[i] = new int[i + 1];
-			for(int j=0; j<a[i].length; j++) {
-				if(j==0|| j==a[i].length-1) {
-					a[i][j]=1;
+			for (int j = 0; j < a[i].length; j++) {
+				if (j == 0 || j == a[i].length - 1) {
+					a[i][j] = 1;
 				}
 			}
 		}
