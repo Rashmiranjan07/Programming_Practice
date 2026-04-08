8fdfc42367d7c91844555f2e42703203e4524497
Rashmiranjan shaw <heyyrashmiranjan02@gmail.com>
2026-04-08 12:03:32 +0530

    added 23

--- src/com/qsp/day23/SumOfDiagonal.java
+++ src/com/qsp/day23/SumOfDiagonal.java
@@ -25,3 +25,30 @@ class SumOfDiagonal {
 }
 
 
+class SumOfMatrixDiagonals {
+	public static void main(String[] args) {
+		int a[][]= {
+				{4,2,6},
+				{9,2,5},
+				{2,3,9}
+				};
+		int k=0; 
+		int j=a.length-1;
+		int sum=0;
+		for (int i = 0; i < a.length; i++) {
+			sum = sum + a[i][k];
+			if (a.length % 2 == 1 && k == a.length / 2) {
+				k++;
+				j--;
+				continue;
+			}
+			sum = sum + a[i][j];
+			k++;
+			j--;
+		}
+		System.out.println(sum);
+	}
+/*
+23
+ */
+}
