8b7992f70b843c5482f5aec23bdd9bd64a33e1a7
Rashmiranjan shaw <heyyrashmiranjan02@gmail.com>
2026-04-08 11:39:09 +0530

    added 23

---
+++ src/com/qsp/day23/SumOfDiagonal.java
@@ -0,0 +1,14 @@
+package com.qsp.day23;
+
+class SumOfDiagonal {
+	public static void main(String[] args) {
+		int a[][]= {
+				{4,2,6,1},
+				{9,2,5,1},
+				{2,3,9,1},
+				{8,3,5,4}
+				};
+		}
+	}
+
+
