743f23206435436d6e5e29907bda98dbdae7ce94
Rashmiranjan shaw <heyyrashmiranjan02@gmail.com>
2026-04-09 10:55:07 +0530

    added day24

--- src/com/qsp/day24/MaxElementLast.java
+++ src/com/qsp/day24/MaxElementLast.java
@@ -5,18 +5,18 @@ import java.util.Arrays;
 class MaxElementLast {
 	public static void main(String[] args) {
 		int a[] = { 9, 2, 4, 1, 6, 3, 2, 8 };
-soluction(a);
+		soluction(a);
 	}
-	
+
 	static void soluction(int a[]) {
-		for( int j=0; j<a.length; j++) {
-			if(a[j]>a[j+1]) {
-				int temp=a[j];
-				a[j]=a[j+1];
-				a[j+1]=temp;
+		for (int j = 0; j < a.length; j++) {
+			if (a[j] > a[j + 1]) {
+				int temp = a[j];
+				a[j] = a[j + 1];
+				a[j + 1] = temp;
 			}
-			
+
 		}
-System.out.println(Arrays.toString(a));
+		System.out.println(Arrays.toString(a));
 	}
 }
