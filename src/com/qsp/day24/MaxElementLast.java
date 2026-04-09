56bfe4e0fd2028152f3d2dac031c5b251d514bb8
Rashmiranjan shaw <heyyrashmiranjan02@gmail.com>
2026-04-09 10:50:31 +0530

    added day24

--- src/com/qsp/day24/MaxElementLast.java
+++ src/com/qsp/day24/MaxElementLast.java
@@ -6,12 +6,6 @@ class MaxElementLast {
 
 	}
 	static void soluction(int arr[]) {
-		int max = arr[0];
-
-		for (int i = 0; i < arr.length; i++) {
-			if (arr[i] > max) {
-				max = arr[i];
-			}
-		}
+		
 	}
 }
