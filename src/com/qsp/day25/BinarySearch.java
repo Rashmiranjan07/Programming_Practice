3367ad3dbd3ef1307272eb56179a02cefac19d5c
Rashmiranjan shaw <heyyrashmiranjan02@gmail.com>
2026-04-10 11:24:58 +0530

    added day25

--- src/com/qsp/day25/BinarySearch.java
+++ src/com/qsp/day25/BinarySearch.java
@@ -3,7 +3,23 @@ package com.qsp.day25;
 class BinarySearch {
 	public static void main(String[] args) {
 		int a[] = { 1, 3, 4, 5, 7, 8, 10, 12 };
+		soluction(a, 3);
+	}
+
+	static int soluction(int a[], int key) {
+		int start = 0;
+		int end = a.length - 1;
+		while (start <= end) {
+			int mid = (start + end) / 2;
+			if (a[mid] == key) {
+				return mid;
+			} else if (a[mid] < key) {
+				start = mid + 1;
+			} else {
+				end = mid - 1;
+			}
+		}
+		return -1;
 
 	}
-	
 }
