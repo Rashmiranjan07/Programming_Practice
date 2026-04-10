01d52ad1f0d9e8cf918e8d0b8d32fcad586776f7
Rashmiranjan shaw <heyyrashmiranjan02@gmail.com>
2026-04-10 12:00:12 +0530

    added day25

--- src/com/qsp/day25/SecondBiggestElement.java
+++ src/com/qsp/day25/SecondBiggestElement.java
@@ -24,3 +24,28 @@ class SecondBiggestElement {
 7
 	 */
 }
+
+
+
+class Secondmax {
+	public static void main(String[] args) {
+		int[] a = { 2,3,5,1,6,2,7,9,9 };
+		System.out.println(sol(a));
+	}
+
+	static int sol(int a[]) {
+		int max = Integer.MIN_VALUE;
+		int secondMax = Integer.MIN_VALUE;
+		for (int i = 0; i < a.length; i++) {
+			if (a[i] > max) {
+				secondMax = max;
+				max = a[i];
+			}
+		}
+		return secondMax;
+	}
+	/*
+7
+	 */
+}
+
