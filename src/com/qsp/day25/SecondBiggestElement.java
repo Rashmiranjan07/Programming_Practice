a9b42fe2a37b8dab0d818706707ace1095bc6ae5
Rashmiranjan shaw <heyyrashmiranjan02@gmail.com>
2026-04-10 11:35:47 +0530

    added day25

--- src/com/qsp/day25/SecondBiggestElement.java
+++ src/com/qsp/day25/SecondBiggestElement.java
@@ -1,5 +1,18 @@
 package com.qsp.day25;
 
 class SecondBiggestElement {
-
+	public static void main(String[] args) {
+		int[] a = { 2, 1, 0, 3, 5, 9, 10, 2, 5, 13, 11 };
+		int max = Integer.MIN_VALUE;
+		int secMax = Integer.MAX_VALUE;
+		for (int temp : a) {
+			if (temp > max) {
+				secMax = max;
+				max = temp;
+			} else if (temp > secMax && temp != max) {
+				secMax = temp;
+			}
+		}
+		System.out.println(secMax);
+	}
 }
